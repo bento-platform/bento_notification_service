15586d3457267e0ebdb0a052cf9b3c029d72db9c
David Lougheed <david.lougheed@gmail.com>
2019-11-29 16:56:33 -0500

    Add notification endpoints

--- chord_notification_service/app.py
+++ chord_notification_service/app.py
@@ -24,6 +24,51 @@ with application.app_context():
         update_db()
 
 
+def notification_dict(notification) -> dict:
+    return {
+        "id": notification[0],
+        "title": notification[1],
+        "description": notification[2],
+        "action_type": notification[3],
+        "action_target": notification[4],
+        "read": bool(notification[5]),
+    }
+
+
+@application.route("/notifications", methods=["GET"])
+def notification_list():
+    c = get_db().cursor()
+    c.execute("SELECT * FROM notifications")
+    return jsonify([notification_dict(n) for n in c.fetchall()])
+
+
+@application.route("/notifications/<int:notification_id>", methods=["GET"])
+def notification_detail(notification_id: int):
+    c = get_db().cursor()
+    c.execute("SELECT * FROM notifications WHERE id = ?", (notification_id,))
+    notification = c.fetchone()
+
+    if notification is None:
+        return application.response_class(status=404)
+
+    return notification_dict(notification)
+
+
+@application.route("/notifications/<int:notification_id>/read", methods=["POST"])
+def notification_read(notification_id: int):
+    db = get_db()
+    c = db.cursor()
+
+    c.execute("SELECT * FROM notifications WHERE id = ?", (notification_id,))
+    if c.fetchone() is None:
+        return application.response_class(status=404)
+
+    c.execute("UPDATE notifications SET read = 1 WHERE id = ?", (notification_id,))
+    db.commit()
+
+    return application.response_class(status=201)
+
+
 @application.route("/service-info", methods=["GET"])
 def service_info():
     # Spec: https://github.com/ga4gh-discovery/ga4gh-service-info
