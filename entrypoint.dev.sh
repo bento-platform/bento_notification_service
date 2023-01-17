12cbd386ad6624c8e273059abec7aacd91ca3160
David Lougheed <david.lougheed@gmail.com>
2023-01-17 16:42:33 -0500

    fix: add migration command to dev entrypoint

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -10,4 +10,9 @@ fi
 
 # Install module locally (similar to pip install -e: "editable mode")
 poetry install
+
+# Run migrations, if needed
+flask db upgrade
+
+# Start Flask + debugger
 python -m debugpy --listen 0.0.0.0:5678 -m flask run --host 0.0.0.0 --port "${INTERNAL_PORT}"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-cd bento_notification_service || exit
-flask db upgrade
-cd .. || exit
+export FLASK_APP=bento_notification_service.app:application
 
 if [ -z "${INTERNAL_PORT}" ]; then
   # Set default internal port to 5000
   INTERNAL_PORT=5000
 fi
 
+# Run migrations, if needed
+flask db upgrade
+
+# Start API server
 # shellcheck disable=SC2003
 # shellcheck disable=SC2046
 gunicorn bento_notification_service.app:application \
