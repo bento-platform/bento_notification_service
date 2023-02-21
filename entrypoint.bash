7c5fcb351e5d586f99bcfb9e8d019517b834ff22
David Lougheed <david.lougheed@gmail.com>
2023-02-21 09:52:06 -0500

    chore: use base image script to generate service user

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# If set, use the local UID from outside the container (or default to 1000)
-USER_ID=${BENTO_UID:-1000}
-
-echo "[bento_notification_service] [entrypoint] starting with USER_ID=${USER_ID}"
-
-# Create service user
-useradd --shell /bin/bash -u "${USER_ID}" --non-unique -c "Bento container user" -m bento_user
-export HOME=/home/bento_user
+# Create bento_user + home
+source /create_service_user.bash
 
 # Fix permissions on /notification
 chown -R bento_user:bento_user /notification
